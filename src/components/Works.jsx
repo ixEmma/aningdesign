c9a7d666e49d8203dfacf8aaf9c1c3aaa46131c1
Micap Steel <you@example.com>
2025-12-25 17:29:39 -0800

    scrol-fix

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -13,16 +13,13 @@ function Works() {
           </div>
           {/* repeat again for seamless loop */}
           <div className="work-card">
-            <img src="/images/work2.jpg" alt="Work 2" />
+            <img src="/images/work3.jpg" alt="Work 2" />
           </div>
           <div className="work-card">
             <img src="/images/work1.jpg" alt="Work 1" />
           </div>
           <div className="work-card">
-            <img src="/images/work1.jpg" alt="Work 1" />
-          </div>
-          <div className="work-card">
-            <img src="/images/work1.jpg" alt="Work 1" />
+            <img src="/images/work3.jpg" alt="Work 1" />
           </div>
           <div className="work-card">
             <img src="/images/work2.jpg" alt="Work 2" />
